1fae62b145b7d1ec91ddda86d23934e5ef8256c4
hrtdss <hurtadoss74@gmail.com>
2023-08-15 17:58:43 +0500

    url upd

--- src/services/Constants.js
+++ src/services/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://f09f-92-43-185-26.ngrok-free.app/api' 
+export const API_URL = 'https://2452-92-43-185-26.ngrok-free.app/api' 
 // 'http://192.168.0.108:81/api' // 'http://localhost:8081/api' // 'https://localhost:7183/api'
